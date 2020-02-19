0a671998154f3c85737afcbdc35dc82b98d89704
safeeee <61018874+safeeee@users.noreply.github.com>
2020-02-19 09:17:11 -0400

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,54 @@
+#
+#            _      _        _____ _   _    ____  _   _ ______
+#     /\   | |    | |      |_   _| \ | |  / __ \| \ | |  ____|
+#    /  \  | |    | |        | | |  \| | | |  | |  \| | |__
+#   / /\ \ | |    | |        | | | . ` | | |  | | . ` |  __|
+#  / ____ \| |____| |____   _| |_| |\  | | |__| | |\  | |____
+# /_/    \_\______|______| |_____|_| \_|  \____/|_| \_|______|
+#
+#
+#    ~ A Fuck Ton Of Tools For Hacking In One Place by SAFE ~
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/allinone/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/safe
+
+clear
+
+mkdir safe
+clear
+
+cd safe
+clear
+
+git clone https://github.com/safeeee/allinone.git
+
+clear
+
+cd allinone
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
